c14ee0b9390767c90a413f8759ecd778a03fa357
Danisan <daniel@blancomartin.cl>
2015-09-30 03:22:19 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Chilean Localization',
+    'version': '1.0',
+    'category': 'Chilean Localization',
+    'sequence': 14,
+    'summary': 'localization, chile, configuration',
+    'description': """
+    """,
+    'author':  u'Blanco Martín & Asociados',
+    'website': 'http://blancomartin.cl',
+    'images': [
+    ],
+    'depends': [
+        'base_setup'
+    ],
+    'data': [
+        'l10n_cl_base_groups.xml',
+        'res_config_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
