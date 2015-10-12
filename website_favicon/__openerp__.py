2b8ab64308db37a24a5263bcdc358ccfbf1bfc70
Danisan <daniel@blancomartin.cl>
2015-10-12 17:28:32 -0300

    first commit

---
+++ website_favicon/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Website Favicon",
+    'version': '1.0.0',
+    'author': 'Blanco Martin & Asociados',
+    'category': 'Custom',
+    'website': 'https://blancomartin.cl',
+    'description': '''Allows you to change the Favicon,
+from both sides, website, and web''',
+    'depends': ['website'],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True
+}
