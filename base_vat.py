6aff8ad42c35ce64edad1663b1929293e29422fb
Danisan <daniel@blancomartin.cl>
2015-11-10 03:26:09 -0300

    [FIX] correction

--- base_vat.py
+++ base_vat.py
@@ -1,29 +1,19 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
-import locale
-
-# locale.setlocale(locale.LC_ALL,'es_CL.utf-8')
+import re
 
 
 class res_partner(models.Model):
     _inherit = 'res.partner'
 
     formated_vat = fields.Char(
-        translate=True, string='Printable VAT', compute='_get_formated_vat',
+        translate=True, string='Printable VAT', compute='_get_f_vat',
         store=True, help='Show formatted vat')
 
-    @api.multi
-    def _get_formated_vat(self):
+    @api.depends('vat')
+    def _get_f_vat(self, vat):
         """
         Retorna el RUT formateado en forma acostumbrada (xx.xxx.xxx-x).
         """
-        res = {}
-        for partner in self:
-            res[partner.id] = self.format_vat_cl(partner.vat)
-        return res
-
-    def format_vat(self,vat):
-        try:
-            return locale.format('%d',int(float(vat[2:10])),1)+'-' + vat[10:]
-        except:
-            return "            "
+        for record in self:
+            record.formated_vat = record.document_number
