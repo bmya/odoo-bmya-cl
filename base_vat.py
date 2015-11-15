beb74742a9d9deec85ae6ecf3059de7fd09595a5
Danisan <daniel@blancomartin.cl>
2015-11-15 19:29:31 -0300

    [FIX] Dependency fixes

--- base_vat.py
+++ base_vat.py
@@ -7,13 +7,16 @@ class res_partner(models.Model):
     _inherit = 'res.partner'
 
     formated_vat = fields.Char(
-        translate=True, string='Printable VAT', compute='_get_f_vat',
+        translate=True, string='Printable VAT',
         store=True, help='Show formatted vat')
 
-    @api.depends('vat')
-    def _get_f_vat(self):
-        """
-        Retorna el RUT formateado en forma acostumbrada (xx.xxx.xxx-x).
-        """
-        for record in self:
-            record.formated_vat = record.document_number
+    @api.onchange('formated_vat')
+    def onchange_document(self):
+        mod_obj = self.env['ir.model.data']
+        formated_vat = (
+            re.sub('[^1234567890Kk]', '',
+            str(self.formated_vat))).zfill(9).upper()
+
+        self.vat = 'CL%s' % formated_vat
+        
+        self.formated_vat = formated_vat[0:2] + '.' + formated_vat[2:5] + '.' + formated_vat[5:8] +'-' + formated_vat[-1]
