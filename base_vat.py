1465e8596a8e6d79f8a65d676d84214da3658a0d
Danisan <daniel@blancomartin.cl>
2016-02-03 01:34:41 -0300

    Add check_vat in order to be independent to l10n_cl_vat in Odoo 9

--- base_vat.py
+++ base_vat.py
@@ -10,6 +10,25 @@ class res_partner(models.Model):
         translate=True, string='Printable VAT',
         store=True, help='Show formatted vat')
 
+
+    def check_vat_cl(self, vat):
+        body, vdig = '', ''
+        if len(vat) > 9:
+            vat = vat.replace('-','',1).replace('.','',2)
+        if len(vat) != 9:
+            return False
+        else:
+            body, vdig = vat[:-1], vat[-1].upper()
+        try:
+            vali = range(2,8) + [2,3]
+            operar = '0123456789K0'[11 - (sum([int(digit)*factor for digit, factor in zip(body[::-1],vali)]) % 11)]
+            if operar == vdig:
+                return True
+            else:
+                return False
+        except IndexError:
+
+
     @api.onchange('formated_vat')
     def onchange_document(self):
         mod_obj = self.env['ir.model.data']
@@ -18,5 +37,5 @@ class res_partner(models.Model):
             str(self.formated_vat))).zfill(9).upper()
 
         self.vat = 'CL%s' % formated_vat
-        
+
         self.formated_vat = formated_vat[0:2] + '.' + formated_vat[2:5] + '.' + formated_vat[5:8] +'-' + formated_vat[-1]
