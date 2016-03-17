38aaac7ae56c82d4ea1ca5f99c8af7adb1247764
Danisan <daniel@blancomartin.cl>
2016-03-17 18:16:11 -0300

    Reorganization of module

--- models/__init__.py
+++ models/__init__.py
@@ -28,4 +28,5 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
-from . import models
+from . import partner_activities
+from . import invoice_turn
