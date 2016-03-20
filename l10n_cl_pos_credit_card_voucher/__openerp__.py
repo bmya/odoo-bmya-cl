6bf91c3317cfcb0949b3cc4ebcf4dd9e2c247fbd
Danisan <daniel@blancomartin.cl>
2016-03-20 03:50:10 -0300

    Merged l10n_cl_pos_credit_card_voucher

---
+++ l10n_cl_pos_credit_card_voucher/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Chilean Localization - Point of sale credit card voucher",
+    'version': "1.0",
+    'license': 'AGPL-3',
+    'author': "Daniel Blanco Martin - Blanco Martín & Asociados",
+    'website': "http://blancomartin.cl",
+    'category': "Tools",
+    'depends': ['point_of_sale'],
+    'version': '0.1',
+    'description': """
+""",
+    'data': ['views.xml'],
+    'qweb': ['static/src/xml/pos_cc_voucher.xml'],
+    'installable': True,
+    'category': 'Localization/Chile',
+}
