d28e76efba836c9e4f58fdad928665a69c03f2f3
Danisan <daniel@blancomartin.cl>
2016-03-30 09:13:04 -0300

    [IMP]  Partner Activity in invoice: one of the activities is selected by default (the last).

--- models/invoice_turn.py
+++ models/invoice_turn.py
@@ -29,15 +29,30 @@
 #
 ##############################################################################
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class invoice_turn(models.Model):
     _inherit = "account.invoice"
+
+
+    @api.multi
+    @api.depends('partner_id')
+    def _get_available_turns(self):
+        self.ensure_one()
+        available_turn_ids = self.partner_id.partner_activities_ids
+        for turn in available_turn_ids:
+            self.invoice_turn = turn.id
+
 
     invoice_turn = fields.Many2one(
         'partner.activities',
         'Giro Receptor',
         readonly=True,
         store=True,
-        states={'draft': [('readonly', False)]})
+        states={'draft': [('readonly', False)]},
+        compute=_get_available_turns)
+
+
+
+
