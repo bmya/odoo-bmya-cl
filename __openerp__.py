2e373d9015fb287c742299716af553a8b86fc650
nelsonramirezs <yo@nelsonramirez.com>
2016-04-12 14:01:11 -0300

    security updates

--- __openerp__.py
+++ __openerp__.py
@@ -47,6 +47,7 @@ Chilean Payroll Salary Rules.
         'views/report_hrsalarybymonth.xml',
         'views/report_payslip.xml',
         'views/hr_indicadores_previsionales_view.xml',
+
         'data/l10n_cl_hr_payroll_data.xml',
         'demo/l10n_cl_hr_payroll_demo.xml',
     ],
