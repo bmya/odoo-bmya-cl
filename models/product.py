67ee228c98bd3b764c84517c422a464c1e561663
Daniel Blanco <daniel@blancomartin.cl>
2016-09-24 15:44:01 -0300

    [IMP] Added concept of sale tax exempt product. to simplify tax analysis during dte dictionary generation.

--- models/product.py
+++ models/product.py
@@ -1,23 +1,43 @@
 # -*- coding: utf-8 -*-
-from openerp import fields, models
+from openerp import fields, models, api
 
 
-class product_template(models.Model):
+class productTemplate(models.Model):
     _inherit = 'product.template'
 
-    type = fields.Selection(
-        [('adjust', 'Adjust concept'),
-         ('consu', 'Consumable'),
-         ('service', 'Service')], 'Product Type', required=True,
-            help="Adjust concept are items to be incremented in the client \
-            account by invoice errors, interest or other reasons, consumable \
-            are product where you don't manage stock, a service is a \
-            non-material product provided by a company or an individual.")]
-    is_excempt = fields.Boolean('Excempt Product', default=False)
+    # Esto no sé que es... estaba hecho, pero estaba sin aplicar
+    # porque no llamaba al código este desde el __init__
+    # type = fields.Selection(
+    #     [('adjust', 'Adjust concept'),
+    #      ('consu', 'Consumable'),
+    #      ('service', 'Service')], 'Product Type', required=True,
+    #         help="Adjust concept are items to be incremented in the client \
+    #         account by invoice errors, interest or other reasons, consumable \
+    #         are product where you don't manage stock, a service is a \
+    #         non-material product provided by a company or an individual.")]
 
-    @api.onchange('is_excempt')
+    # TODO: mejorar la vista, de tal manera que si el producto es exento los
+    # impuestos no los permita editar
+    # TODO: cambiar la vista, para que los impuestos no se puedan poner en la
+    # linea del producto
+    # por ahora, l10n_cl_dte busca los asserts que permitan validar que lo que
+    # se hace está bien.
+    is_exempt = fields.Boolean('Sales Tax Exempt', default=False)
+
+    @api.onchange('is_exempt')
     def _reset_tax(self):
-        pass
+        if self.is_exempt:
+            self.taxes_id = False
 
 
+class productProduct(models.Model):
+    _inherit = 'product.product'
 
+    is_exempt = fields.Boolean(
+        'Sales Tax Exempt', default=False,
+        related='product_tmpl_id.is_exempt')
+
+    @api.onchange('is_exempt')
+    def _reset_tax(self):
+        if self.is_exempt:
+            self.taxes_id = False
